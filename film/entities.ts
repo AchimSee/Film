ef46d43bc515785410303b92f4240afb80e174ab
AchimSee <102674324+AchimSee@users.noreply.github.com>
2023-10-29 19:15:34 +0100

    Remove Film

--- film/entities.ts
+++
@@ -1,6 +0,0 @@
-import { Film } from './film.entity.js';
-import { Titel } from './titel.entity.js';
-import { Schauspieler } from './schauspieler.entity.js';
-
-// erforderlich in src/config/db.ts und src/buch/buch.module.ts
-export const entities: [Film, Titel, Schauspieler];
